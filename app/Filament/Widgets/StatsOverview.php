9a507a738a01184980a69da6cd4c58bac3b5412c
P0Ci <febriandry58@gmail.com>
2025-02-12 17:16:58 +0700

    Penambahan Filter Tanggal

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -3,25 +3,37 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Transaction;
+use Carbon\Carbon;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use PHPUnit\Event\Tracer\Tracer;
 
 class StatsOverview extends BaseWidget
 {
+    use InteractsWithPageFilters;
+
     protected function getStats(): array
     {
-        $pemasukan = Transaction::incomes()->get()->sum('amount');
-        $pengeluaran = Transaction::expenses()->get()->sum('amount');
+        $startDate = ! is_null($this->filters['startDate'] ?? null) ?
+            Carbon::parse($this->filters['startDate']) :
+            null;
+
+        $endDate = ! is_null($this->filters['endDate'] ?? null) ?
+            Carbon::parse($this->filters['endDate']) :
+            now();
+
+
+        $pemasukan = Transaction::incomes()
+                        ->whereBetween('date_transaction', [$startDate, $endDate])
+                        ->sum('amount');
+        $pengeluaran = Transaction::expenses()
+                        ->whereBetween('date_transaction', [$startDate, $endDate])
+                        ->sum('amount');
         return [
-            Stat::make('Total Pemasukan', $pemasukan)
-            ->description('32k increase')
-            ->descriptionIcon('heroicon-m-arrow-trending-up'),
-            Stat::make('Total Pengeluaran', $pengeluaran)
-            ->description('7% decrease')
-            ->descriptionIcon('heroicon-m-arrow-trending-down'),
-            Stat::make('Selisih', $pemasukan - $pengeluaran)
-            ->description('3% increase')
-            ->descriptionIcon('heroicon-m-arrow-trending-up'),
+            Stat::make('Total Pemasukan', 'Rp.'.' '.$pemasukan),
+            Stat::make('Total Pengeluaran', 'Rp.'.' '. $pengeluaran),
+            Stat::make('Selisih', 'Rp.'.' '. $pemasukan - $pengeluaran),
         ];
     }
 }
